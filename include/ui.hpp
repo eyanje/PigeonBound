4110220d2a961cf68beb3d2c844801bf785a40be
Edward Yan <eyanje@gmail.com>
2019-01-04 02:56:19 -0500

    Created a downloadable version

--- include/ui.hpp
+++ include/ui.hpp
@@ -11,33 +11,23 @@
 #include "sprite.hpp"
 
 struct FontFace {
+    static FT_Library library;
     FT_Face face;
     std::map<char, Image> images;
 
-    FontFace(std::string path) {
-        FT_New_Face(library,
-        path.c_str(),
-        0,
-        &face);
-
-        FT_Set_Char_Size(
-        face,
-        0, // char_width in 1/64th of points
-        16*64,   // char_height in 1/64th of points
-        300, 300); // Resolution
-    }
+    FontFace(std::string path);
+    
+    static void init();
 };
 
 class Text {
     private:
-        static FT_Library library;
         static std::map<std::string, FontFace> faces;
         std::string text;
         std::string font;
         void renderChar(const char c, const int x, const int y);
     public:
         Text(const std::string text, const std::string font = "default.ttf");
-        static void init();
         void render(const int x, const int y, int width);
 };
 
--- src/ui.cpp
+++ src/ui.cpp
@@ -6,12 +6,26 @@
 
 #include "sprite.hpp"
 
-FT_Library Text::library;
-std::map<std::string, FontFace> Text::faces;
+FT_Library FontFace::library;
 
-void Text::init() {
+FontFace::FontFace(std::string path)  {
+    FT_New_Face(library,
+    path.c_str(),
+    0,
+    &face);
+
+    FT_Set_Char_Size(
+    face,
+    0, // char_width in 1/64th of points
+    16*64,   // char_height in 1/64th of points
+    300, 300); // Resolution
+}
+
+void FontFace::init() {
     FT_Init_FreeType(&library);
 }
+
+std::map<std::string, FontFace> Text::faces;
 
 Text::Text(const std::string text, const std::string font)
 : text(text),
@@ -22,8 +36,8 @@ font(font) {
 }
 
 void Text::renderChar(const char c, const int x, const int y) {
-    FT_Load_Glyph(faces[font].face, c, FT_LOAD_DEFAULT | FT_LOAD_RENDER);
-    FT_Render_Glyph(faces[font].face->glyph, FT_RENDER_MODE_NORMAL);
+    FT_Load_Glyph(faces.at(font).face, c, FT_LOAD_DEFAULT | FT_LOAD_RENDER);
+    FT_Render_Glyph(faces.at(font).face->glyph, FT_RENDER_MODE_NORMAL);
 
 }
 
@@ -32,9 +46,9 @@ void Text::render(const int x, const int y, const int width) {
     int yLoc = y;
     for (char c : text) {
         renderChar(c, xLoc, yLoc);
-        xLoc += (faces[font].face->glyph->advance.x >> 6);
-        if (xLoc + (faces[font].face->glyph->advance.x >> 6) > x + width && width) {
-            yLoc += (faces[font].face->glyph->advance.y >> 6);
+        xLoc += (faces.at(font).face->glyph->advance.x >> 6);
+        if (xLoc + (faces.at(font).face->glyph->advance.x >> 6) > x + width && width) {
+            yLoc += (faces.at(font).face->glyph->advance.y >> 6);
             xLoc = x;
         }
     }
